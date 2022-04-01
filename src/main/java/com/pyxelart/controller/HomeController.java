0c7387e10779595d41b23b2a755ee8c2b08183a3
Gabriel <Gabriel8819@hotmail.com>
2022-04-01 15:29:35 -0400

    first commit

---
+++ src/main/java/com/pyxelart/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.pyxelart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	
+	@GetMapping("/home")
+	public String getHome() {
+		return "test result";
+	}
+	
+	
+	
+}
