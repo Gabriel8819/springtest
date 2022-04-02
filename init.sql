81e8f12f991e99743b9ddc7261febbd0e362f90a
Gabriel <Gabriel8819@hotmail.com>
2022-04-01 23:25:46 -0400

    tt

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 create table user(
 	id bigint primary key auto_increment,
 	password varchar(255) not null,
-	username varchar(255) not null;
+	username varchar(255) not null
 );
 
 insert into user (username, password) values ("charlie", "secrt-unicorn");
