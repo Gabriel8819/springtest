132c36ea061b322924d51587750d0bfda543e044
Gabriel <Gabriel8819@hotmail.com>
2022-04-01 22:01:49 -0400

    add mysql

--- src/main/java/com/pyxelart/controller/HomeController.java
+++ src/main/java/com/pyxelart/controller/HomeController.java
@@ -1,15 +1,29 @@
 package com.pyxelart.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pyxelart.UserRepo;
+import com.pyxelart.model.User;
+
 @RestController
 public class HomeController {
+	
+	@Autowired
+	UserRepo userRepo;
 	
 	
 	@GetMapping("/home")
 	public String getHome() {
 		return "test result";
+	}
+	
+	
+	@GetMapping("/user")
+	public User getUser() {
+		return this.userRepo.findById(1L).get();
+		
 	}
 	
 	
